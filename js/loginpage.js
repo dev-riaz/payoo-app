f84a92b87d0143f8ac924ef16ebcd0a311418d36
riaz113 <riazmahamud113@gmail.com>
2026-03-09 18:39:17 +0600

    login page complet

---
+++ js/loginpage.js
@@ -0,0 +1,16 @@
+document.getElementById('login-btn').addEventListener('click', function () {
+    const inputNumber = document.getElementById('input-number');
+    const inputValue = inputNumber.value;
+
+    const inputPin = document.getElementById('input-pin');
+    const inputPinValue = inputPin.value;
+
+    if (inputValue === '01312121212' && inputPinValue === '1234') {
+        alert('login Successful');
+        window.location.assign("/home.html")
+    } else {
+        alert('login Failed');
+        return;
+    }
+
+})
